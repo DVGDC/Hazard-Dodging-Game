92c7128a3b0a510a0d06d8eea87fa862f3191c09
Stan Man <stanley.df.zhao@gmail.com>
2024-10-03 22:18:24 -0700

    Restored original project and added comments to PlayerMovement script

--- Dodge Game/Assets/PlayerMovement.cs
+++ Dodge Game/Assets/PlayerMovement.cs
@@ -4,23 +4,61 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+	// Float values for player movement speed and jump force
+	[SerializeField] private float speed;
+	[SerializeField] private float jumpForce;
 
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpForce;
+	// Stores referrence to player's Rigidbody2D component
+	private Rigidbody2D rb;
 
-    private Rigidbody2D rb;
+	// Inspector fields for groundcheck to check whether or not the player is grounded
+	[SerializeField] private Transform groundCheck;
+	[SerializeField] private float groundCheckRadius;
+	[SerializeField] private LayerMask groundMask;
 
-    [SerializeField] private Transform groundCheck;
+	// The time window in which a jump input will be buffered
+	[SerializeField] private float jumpBufferWindow;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+	// Stores current value of jump buffer in seconds until buffer window is over
+	// If greater than zero, we know that a jump input is currently buffered
+	private float jumpBufferCountdown;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called upon scene initialization
+	void Start()
+	{
+		// Assign player's Rigidbody2D component to rb
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		// Check if player is grounded using Physics2D's OverlapCircle method
+		bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
+
+		// Print whether or not player is currently grounded to console (will remove this line later)
+		Debug.Log(isGrounded);
+
+		// Take in horizontal input and multiply it by speed to get the player's velocity for this frame
+		float moveSpeed = Input.GetAxisRaw("Horizontal") * speed;
+
+		// If JumpButton or UpArrow pressed down
+		if (Input.GetButtonDown("JumpButton") || Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			// Buffer a jump input
+			jumpBufferCountdown = jumpBufferWindow;
+		}
+
+		// Maintain the time window for the buffered jump input
+		jumpBufferCountdown -= Time.deltaTime;
+
+		// If jump input is buffered
+		if (jumpBufferCountdown > 0 && isGrounded)
+		{
+			// Set vertical velocity of player to jumpForce
+			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+		}
+
+		// Set horizontal velocity to this frame's moveSpeed
+		rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+	}
 }
