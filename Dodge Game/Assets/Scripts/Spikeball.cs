bc4c3ebb1baae43c6e374baab773b68f5f906b25
Stan Man <stanley.df.zhao@gmail.com>
2024-10-23 20:41:13 -0700

    8:41 commit

--- Dodge Game/Assets/Scripts/Spikeball.cs
+++ Dodge Game/Assets/Scripts/Spikeball.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Spikeball : Hazard
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float rollSpeed;
+	[SerializeField] private float rollTorque;
+
+    private Rigidbody2D rb;
+
+	// Start is called before the first frame update
+	new void Start()
     {
-        
+        base.Start();
+
+        rb = GetComponent<Rigidbody2D>();
+        if(transform.position.x < 0)
+        {
+            rb.AddForce(rollSpeed * Vector2.right, ForceMode2D.Impulse);
+        }
+        else
+        {
+			rb.AddForce(rollSpeed * Vector2.left, ForceMode2D.Impulse);
+		}
+
     }
 
     // Update is called once per frame
